91ae8a2818b4dbc6dddd03c5877e938c6d6aa3e6
Timur Reziapov <timurreziapov2@gmail.com>
2024-11-02 23:14:27 -0700

    adds missing dot_env import

--- storage/drop_tables_mysql.py
+++ storage/drop_tables_mysql.py
@@ -1,8 +1,15 @@
 import mysql.connector
 import yaml
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 with open('app_conf.yaml', 'r') as f:
     app_config = yaml.safe_load(f.read())
+    app_config["datastore"]["hostname"] = os.getenv("KAFKA_HOST_NAME")
+    app_config["datastore"]["user"] = os.getenv("MYSQL_USER")
+    app_config["datastore"]["password"] = os.getenv("MYSQL_PASSWORD")
 
 db_conn = mysql.connector.connect(host=app_config["datastore"]["hostname"], user=app_config["datastore"]["user"], password=app_config["datastore"]["password"], database=app_config["datastore"]["db"])
 
