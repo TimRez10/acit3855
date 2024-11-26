f6366774d3ffed31d3dd9d8518d407935723d929
Timur Reziapov <timurreziapov2@gmail.com>
2024-11-26 09:37:13 -0800

    little fix

--- receiver/app.py
+++ receiver/app.py
@@ -93,6 +93,6 @@ def add_refill_record(body):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml", base_path="/receiver", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
